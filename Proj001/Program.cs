2b56eea99a8a096380117d30f8a7c9a7448104c8
Oleg.Stelm <oleg.stelm@gmail.com>
2022-12-06 22:00:22 +0300

    InitCommit

---
+++ Proj001/Program.cs
@@ -0,0 +1,17 @@
+﻿int a;
+
+Console.WriteLine("enter a number: ");
+int.TryParse(Console.ReadLine(), out a);
+if (a>=100 && a<=999 || a<=-100 && a>=-999)
+{
+    if (a<0)
+    {
+        a=-a;
+    }
+    a=((a-a % 10)/10)%10;
+    Console.Write($"figure number two = {a} ");
+}
+else
+{
+    Console.Write($"WTF? This number is not three digits!");
+}
