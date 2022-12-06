7a01bf14dfac7bbbbd0249dc98a9a047e8cbc35b
Oleg.Stelm <oleg.stelm@gmail.com>
2022-12-06 23:43:44 +0300

    AddProj002

---
+++ Proj002/Program.cs
@@ -0,0 +1,23 @@
+﻿int a;
+
+Console.WriteLine("Enter a number: ");
+int.TryParse(Console.ReadLine(), out a);
+int i=0;
+if (a>=100 || a<=-100)
+{
+    for (i=a; i>999; i=((i-i % 10)/10))
+    {
+        
+    }
+    a=i;
+    if (a<0)
+    {
+        a=-a;
+    }
+    a=a % 10;
+    Console.Write($"figure number three = {a} ");
+}
+else
+{
+    Console.Write($"Really? There is no third number!");
+}
