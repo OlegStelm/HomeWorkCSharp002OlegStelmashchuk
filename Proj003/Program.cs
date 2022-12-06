d4b1287ef0102d67226cc36507939f7911c3671e
Oleg.Stelm <oleg.stelm@gmail.com>
2022-12-06 23:54:23 +0300

    AddProj003

---
+++ Proj003/Program.cs
@@ -0,0 +1,20 @@
+﻿int a;
+
+Console.WriteLine("Enter a number: ");
+int.TryParse(Console.ReadLine(), out a);
+
+if (a<=7 && a>=1)
+{
+    if (a==6 || a==7)
+    {
+        Console.Write("WOW! Today is a day off!");
+    }
+    else
+    {
+        Console.Write("Wake up! Go to work!");
+    }
+}
+else
+{
+    Console.Write("The day of the week with this number has not yet been invented!");
+}
